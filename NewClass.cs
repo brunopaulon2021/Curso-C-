89ff1577019ab2522157ca2273f8477200c3537d
Bruno Paulon <paulonbruno9@gmail.com>
2022-12-23 16:30:10 +0000

    update

---
+++ NewClass.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 
+{
+    public class NewClass
+
+    {
+        
+
+        int number1 = 0;
+        int number2 = 0;
+        int number3 = 0;
+        
+        Console.WriteLine("Informe o Primeiro Numero!");
+        int  number1 = Console.ReadLine();
+        
+        Console.WriteLine("Informe o Segundo Numero!");
+        int number2 = Console.ReadLine();
+        
+        Console.WriteLine("Informe o Terceiro Numero!");
+        int  number3 = Console.ReadLine();
+        
+    }
+    
+}
